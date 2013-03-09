9f9e549f02bd4706a11664d722e433f79eb68299
Kim Lindberg Schwaner <kim.schwaner@gmail.com>
2013-03-09 18:22:42 +0100

    Add spin() method and button actions

---
+++ src/edu/sdu/onearmedbandit/Reel.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.sdu.onearmedbandit;
+
+import android.widget.ImageView;
+
+/**
+ *
+ * @author kls
+ */
+public class Reel
+{
+    public ImageView view;
+    public int idx;
+
+    public Reel()
+    {
+        view = null;
+        idx = 0;
+    }
+}
--- src/edu/sdu/onearmedbandit/SpinActivity.java
+++ src/edu/sdu/onearmedbandit/SpinActivity.java
@@ -1,18 +1,26 @@
 package edu.sdu.onearmedbandit;
 
 import android.app.Activity;
-import android.content.res.Resources;
 import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.Random;
 
-public class SpinActivity extends Activity
+public class SpinActivity extends Activity implements OnClickListener
 {
-    private ImageView[] reel;
-    String fruit_strings[];
-    TextView textv;
-    Resources res;
+    private final static String TAG = "edu.sdu.onearmedbandit";
+    private Button bStart;
+    private Button[] bStop;
+    private TextView tViewone;
+    private ArrayList<Drawable> fruits;
+    private Reel[] reels;
 
     /* Called when the activity is first created. */
     @Override
@@ -20,33 +28,73 @@ public class SpinActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.spinactivity);
-        setTitle(this.titleText());
-        res = getResources();
+        setTitle(titleText());
 
-        textv = (TextView) findViewById(R.id.textView);
+        Log.v(TAG, "Initializing ...");
 
-        // Set ImageViews for the three reels
-        reel = new ImageView[3];
-        reel[0] = (ImageView) findViewById(R.id.reel0);
-        reel[1] = (ImageView) findViewById(R.id.reel1);
-        reel[2] = (ImageView) findViewById(R.id.reel2);
+        // Get interface elements
+        reels = new Reel[3];
+        for (int i=0; i<reels.length; i++) {reels[i] = new Reel();}
+        bStop = new Button[3];
+        reels[0].view = (ImageView) findViewById(R.id.reel0);
+        reels[1].view = (ImageView) findViewById(R.id.reel1);
+        reels[2].view = (ImageView) findViewById(R.id.reel2);
+        tViewone = (TextView) findViewById(R.id.textView);
+        bStart = (Button) findViewById(R.id.button_start);
+        bStop[0] = (Button) findViewById(R.id.button_stop_0);
+        bStop[1] = (Button) findViewById(R.id.button_stop_1);
+        bStop[2] = (Button) findViewById(R.id.button_stop_2);
 
+        // Attach button listeners
+        for (int i=0; i<bStop.length; i++) {bStop[i].setOnClickListener(this);}
+        bStart.setOnClickListener(this);
 
-
-
-
-        TypedArray fruits = res.obtainTypedArray(R.array.fruits);
-
-        for (int i=0; i<reel.length; i++)
+        // Construct array of fruit drawables
+        TypedArray typedfruits = getResources().obtainTypedArray(R.array.fruits);
+        fruits = new ArrayList<Drawable>();
+        for (int i=0; i<typedfruits.length(); i++)
         {
-            reel[i].setImageDrawable(fruits.getDrawable(i));
+            fruits.add(typedfruits.getDrawable(i));
+        }
+        fruits.trimToSize();
+
+        // Set random start images
+        Random gen = new Random();
+        for (int i=0; i<reels.length; i++)
+        {
+            int r = gen.nextInt(fruits.size());
+            if (i>1 && r==reels[i-2].idx)
+                {r = (r+3)%fruits.size();} //Ensure they are not all the same
+            reels[i].view.setImageDrawable(fruits.get(r));
+            reels[i].idx = r;
         }
 
-        textv.setText(String.format("Fruit array length: %1$d", fruits.length()));
+        Log.v(TAG, "Done.");
     }
 
+    @Override
+    public void onClick(View v)
+    {
+        switch(v.getId())
+        {
+            case R.id.button_start:
+                for (int i=0; i<reels.length; i++)
+                {
+                    spin(i);
+                }
+                break;
+            case R.id.button_stop_0:
+                break;
+            default:
+                break;
+        }
+    }
 
-
+    private void spin(int i)
+    {
+        reels[i].idx = (reels[i].idx + 1) % fruits.size();
+        reels[i].view.setImageDrawable(fruits.get(reels[i].idx));
+    }
 //    new Thread(new Runnable() {
 //        public void run() {
 //            Bitmap b = loadImageFromNetwork("http://example.com/image.png");
